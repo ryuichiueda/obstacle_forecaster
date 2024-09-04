b9705276b6c67c0fff9f05cb69c78151aafaccfb
Ryuichi Ueda <ryuichiueda@gmail.com>
2024-09-04 19:15:10 +0900

    Add map.cpp

--- include/obstacle_forecaster/map.h
+++ include/obstacle_forecaster/map.h
@@ -1,14 +1,12 @@
 #ifndef MAP_ 
 #define MAP_
 
-using sensor_msgs::msg::LaserScan;
-
 namespace obstacle_forecaster
 {
 
 class Map {
 public:
-	Map(){}
+	Map();
 	int cell_num_x_;
 	int cell_num_y_;
 	double xy_resolution_;
---
+++ src/map.cpp
@@ -0,0 +1,8 @@
+#include "obstacle_forecaster/map.h"
+
+namespace obstacle_forecaster {
+
+Map::Map(){
+}
+
+}
